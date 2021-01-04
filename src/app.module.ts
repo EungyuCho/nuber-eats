5487096146ff367318f91e0d037d78044dfbe1e6
gyu <cho2304@naver.com>
2021-01-04 15:02:46 +0900

    :bookmark: 1.1.0 : Our First Resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import {GraphQLModule} from "@nestjs/graphql";
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { RestaurantsModule } from './restaurans/restaurants.module';
 
 @Module({
   imports: [
-      GraphQLModule.forRoot()
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
