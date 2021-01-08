6fd494c7d1553c72e0cb6f2ceab7a3c7c2316591
gyu <cho2304@naver.com>
2021-01-08 19:23:57 +0900

    :bookmark: 4.2.0 / :sparkles: User Resolver and Service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { UserResolver } from './users.resolver';
+import { UserService } from './users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [],
+  providers: [UserResolver, UserService],
 })
 export class UsersModule {}
